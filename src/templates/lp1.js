fd55b7bd484d647cda9fff860b745e2ff146a9c0
xrxoxcxox <watanuki@ju-zen.com>
2020-10-10 17:07:18 +0900

    Add style to each template

--- src/templates/lp1.js
+++ src/templates/lp1.js
@@ -1,11 +1,54 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { css } from "@emotion/core"
 
-export default function Lp({ data: { lps }}) {
+export default function Lp1({ data: { lps }}) {
+  const cemeteries = lps.properties.map(property => {
+    return(
+      <div key={property.name} css={card}>
+        <img alt='' src={property.image} css={image} />
+        <p css={name}>{property.name}</p>
+        <p>{property.type}</p>
+        <p>{property.address}</p>
+      </div>
+    )
+  })
   return (
-  <h1>{lps.parent.name} type1</h1>
+  <>
+    <h1 css={title}>安さならダントツ</h1>
+    <div css={contents}>
+      {cemeteries}
+    </div>
+  </>
   )
 }
+
+const title = css`
+  font-size: 32px;
+  text-align: center;
+`
+
+const contents = css`
+  padding: 24px 16px;
+`
+
+const card = css`
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.14);
+  padding: 16px;
+  & + & {
+    margin-top: 16px;
+  }
+`
+
+const image = css`
+  width: 100%;
+`
+
+const name = css`
+  font-size: 20px;
+  font-weight: 600;
+`
 
 export const pageQuery = graphql`
   query LpType1($id: String){
--- src/templates/lp2.js
+++ src/templates/lp2.js
@@ -1,11 +1,62 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { css } from "@emotion/core"
 
-export default function Lp({ data: { lps }}) {
+export default function Lp1({ data: { lps }}) {
+  const cemeteries = lps.properties.map(property => {
+    return(
+      <div key={property.name} css={card}>
+        <img alt='' src={property.image} css={image} />
+        <div css={text}>
+          <p css={name}>{property.name}</p>
+          <p>{property.type}</p>
+          <p>{property.address}</p>
+        </div>
+      </div>
+    )
+  })
   return (
-  <h1>{lps.parent.name} type2</h1>
+  <>
+    <h1 css={title}>品揃え一番</h1>
+    <div css={contents}>
+      {cemeteries}
+    </div>
+  </>
   )
 }
+
+const title = css`
+  font-size: 24px;
+  padding: 16px;
+`
+
+const contents = css`
+  padding: 24px 16px;
+`
+
+const card = css`
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.14);
+  display: flex;
+  padding: 16px;
+  & + & {
+    margin-top: 16px;
+  }
+`
+
+const image = css`
+  object-fit: cover;
+  width: 80px;
+`
+
+const text = css`
+  margin-left: 8px;
+`
+
+const name = css`
+  font-size: 20px;
+  font-weight: 600;
+`
 
 export const pageQuery = graphql`
   query LpType2($id: String){
